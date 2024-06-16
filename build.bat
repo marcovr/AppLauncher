111d6e7d817c271a48c0cdc28f3b5d091d53c0cc
Marco vR <marcovr@live.de>
2024-06-16 17:11:42 +0200

    Make it more flexible

--- build.bat
+++ build.bat
@@ -1,2 +1,6 @@
-call "C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\VC\Auxiliary\Build\vcvarsall.bat" x64
-msbuild applauncher.vcxproj
+@echo off
+if "%WindowsSdkDir%"=="" (
+    call "C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\VC\Auxiliary\Build\vcvarsall.bat" x64
+)
+
+msbuild src/applauncher.vcxproj /property:Configuration=Release
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,28 @@
 #include <Windows.h>
 #include <shobjidl.h>
+#include <shlwapi.h>
+#include <comdef.h>
+#include <string>
+#include <sstream>
 
 int main()
 {
+	int argc = 0;
+	LPWSTR cmdLine = GetCommandLineW();
+    LPWSTR* argv = CommandLineToArgvW(cmdLine, &argc);
+
+	if (argc == 1)
+	{
+		MessageBoxW(
+			nullptr,
+			L"Please pass an App Id as first argument.\nFor example: \"dotPDNLLC.paint.net_h55e3w7q8jbva!dotPDNLLC.paint.net\"",
+			argv[0],
+			MB_OK | MB_ICONINFORMATION | MB_TASKMODAL);
+
+		LocalFree(argv);
+		return 1;
+	}
+
 	HRESULT hr = ::CoInitializeEx(nullptr, COINIT_APARTMENTTHREADED);
 	if (SUCCEEDED(hr))
 	{
@@ -13,19 +33,32 @@ int main()
 			CLSCTX_LOCAL_SERVER,
 			IID_IApplicationActivationManager,
 			(LPVOID*)&pApplicationActivationManager);
+
 		if (SUCCEEDED(hr))
 		{
 			// This call ensures that the app is launched as the foreground window
-			hr = ::CoAllowSetForegroundWindow(
-				pApplicationActivationManager, 
-				nullptr);
+			hr = ::CoAllowSetForegroundWindow(pApplicationActivationManager, nullptr);
 			if (SUCCEEDED(hr))
 			{
+				LPWSTR appId = argv[1];
+				LPWSTR args = nullptr;
 				DWORD dwProcessId = 0;
+
+				if (argc > 2)
+				{
+					args = StrStrW(cmdLine, argv[2]);
+
+					// Include quote if present
+					if (args != nullptr && args[-1] == '"')
+					{
+						args--;
+					}
+				}
+
 				// Launch the app
 				hr = pApplicationActivationManager->ActivateApplication(
-					L"dotPDNLLC.paint.net_h55e3w7q8jbva!dotPDNLLC.paint.net",
-					nullptr,
+					appId,
+					args,
 					AO_NONE,
 					&dwProcessId);
 			}
@@ -35,6 +68,25 @@ int main()
 
 		::CoUninitialize();
 	}
+
+	if (!SUCCEEDED(hr))
+	{
+		std::wstringstream ss;
+		ss << L"0x" << std::hex << std::uppercase << hr;
+
+		_com_error err(hr);
+		std::wstring msg = std::wstring(err.ErrorMessage());
+		std::wstring a = std::wstring(argv[1]);
+		std::wstring errorMsg = L"Failed to launch: " + a + L"\n\nError: " + msg + L"\nHRESULT: " + ss.str();
+
+		MessageBoxW(
+			nullptr,
+			errorMsg.c_str(),
+			argv[0],
+			MB_OK | MB_ICONERROR | MB_TASKMODAL);
+	}
+
+	LocalFree(argv);
 
     return hr;
 }
